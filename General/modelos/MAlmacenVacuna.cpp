3ee35783e04076614da6d70cccfbea6895f65ba6
Andres Alvarez <andres15alvarez@gmail.com>
2021-06-25 19:00:31 -0400

    Create MAlmacenVacuna class

---
+++ General/modelos/MAlmacenVacuna.cpp
@@ -0,0 +1,5 @@
+#include "MAlmacenVacuna.h"
+
+MAlmacenVacuna::MAlmacenVacuna()
+{
+}
---
+++ General/modelos/MAlmacenVacuna.h
@@ -0,0 +1,15 @@
+#ifndef MAlmacenVacuna_H
+#define MAlmacenVacuna_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class MAlmacenVacuna {
+private:
+    string marca;
+    int cantidad;
+public:
+    MAlmacenVacuna();
+};
+
+#endif
