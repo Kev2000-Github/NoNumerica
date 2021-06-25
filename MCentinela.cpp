4e38d5a4a52f21bd76d22f32cb4a3c9dc4bb6140
emanuelgcq <86500475+emanuelgcq@users.noreply.github.com>
2021-06-25 19:01:42 -0400

    Add files via upload

---
+++ MCentinela.cpp
@@ -0,0 +1,35 @@
+/*
+ * MCentinela.cpp
+ *
+ *  Created on: 25 jun. 2021
+ *      Author: Kloux
+ */
+
+#include "MCentinela.h"
+
+MCentinela::MCentinela() {
+}
+
+
+bool MCentinela::actualizarVacuna(marca, n){
+
+}
+bool MCentinela::agregarCubiculo(){
+
+}
+bool MCentinela::removerCubiculo(){
+
+}
+
+bool MCentinela::agregarVacuna(){
+
+}
+
+
+bool MCentinela::removerVacuna(){
+
+}
+
+int MCentinela::totalCubiculos(){
+
+}
