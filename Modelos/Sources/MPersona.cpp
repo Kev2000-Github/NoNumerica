2a5de628c5d7664521164364cbf558594f042891
Ana Paulina Calles Azuaje <azuaje43@gmail.com>
2021-06-26 17:03:56 -0400

    personas

---
+++ Modelos/Sources/MPersona.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+#include "MPersona.h"
+
+MPersona::MPersona() {
+}
+void MPersona::setnombre(string n){
+	nombre=n;
+}
+void MPersona::setapellido(string a){
+	apellido=a;
+}
+void MPersona::setcedula(string c){
+	cedula=c;
+}
+string MPersona::getnombre(){
+	return nombre;
+}
+string MPersona::getapellido(){
+	return apellido;
+}
+string MPersona::getcedula(){
+	return cedula;
+}
