be0a669fd2fdb31b91b776998c6e858a912b79b0
Ana Paulina Calles Azuaje <azuaje43@gmail.com>
2021-06-27 12:21:08 -0400

    persona.h

---
+++ Modelos/MPersona.h
@@ -0,0 +1,25 @@
+#ifndef PERSONA_H_INCLUDED
+#define PERSONA_H_INCLUDED
+using namespace std;
+#include <string>
+
+class MPersona{
+
+private:
+        string nombre;
+        string apellido;
+        string cedula;
+
+public:
+         MPersona();
+         void setnombre(string n);
+	     void setapellido(string a);
+	     void setcedula(string c);
+	     string getnombre();
+	     string getapellido();
+	     string getcedula();
+
+};
+
+
+#endif // PERSONA_H_INCLUDED
